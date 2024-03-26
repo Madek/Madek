3ec47ddb6093910ec0903b87a32789ace276f693
uvb <urs.vanbinsbergen@zhdk.ch>
2024-03-26 11:12:56 +0100

    release: v4.4.2

--- dev/release-env.sh
+++ dev/release-env.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -exu
 
-export DEV_INITIALS=mk
+export DEV_INITIALS=uvb
 export RELEASE_MAJOR_MINOR=4.4
-export RELEASE_PATCH=1
+export RELEASE_PATCH=2
 export RELEASE_PRE='' # '-RC.1'; or '' for stable release
 export VERSION_PREFIX='v'
 
