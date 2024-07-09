b5fe36517a716ad2f7e7d1bd41f07056f6b03eea
Matus Kmit <matuskmit1@gmail.com>
2024-07-09 14:23:47 +0200

    release: v4.5.0

--- dev/release-env.sh
+++ dev/release-env.sh
@@ -3,7 +3,7 @@
 export DEV_INITIALS=mk
 export RELEASE_MAJOR_MINOR=4.5
 export RELEASE_PATCH=0
-export RELEASE_PRE='-RC.3' # '-RC.1'; or '' for stable release
+export RELEASE_PRE='' # '-RC.1'; or '' for stable release
 export VERSION_PREFIX='v'
 
 export RELEASE_MAIN="$RELEASE_MAJOR_MINOR.$RELEASE_PATCH"
