c3a4ae04e9f27dfeea03b636fcd37ada67ea05a0
uvb <urs.vanbinsbergen@zhdk.ch>
2025-05-05 10:08:02 +0200

    chore: sync release info (4.8.0)

--- dev/release-env.sh
+++ dev/release-env.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -exu
 
 export DEV_INITIALS=uvb
-export RELEASE_MAJOR_MINOR=4.7
-export RELEASE_PATCH=1
+export RELEASE_MAJOR_MINOR=4.8
+export RELEASE_PATCH=0
 export RELEASE_PRE='' # '-RC.1'; or '' for stable release
 export VERSION_PREFIX='v'
 
