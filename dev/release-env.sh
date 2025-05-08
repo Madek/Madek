337322158c0ed0f343bb215117c606acab753b21
uvb <urs.vanbinsbergen@zhdk.ch>
2025-05-08 15:28:22 +0200

    release: v4.8.1

--- dev/release-env.sh
+++ dev/release-env.sh
@@ -2,7 +2,7 @@
 
 export DEV_INITIALS=uvb
 export RELEASE_MAJOR_MINOR=4.8
-export RELEASE_PATCH=0
+export RELEASE_PATCH=1
 export RELEASE_PRE='' # '-RC.1'; or '' for stable release
 export VERSION_PREFIX='v'
 
