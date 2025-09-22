e94f13088c999cd8c3c4f0bab29137209db6a06e
uvb <urs.vanbinsbergen@zhdk.ch>
2025-09-22 13:17:16 +0200

    release: v4.10.2-RC.1

--- dev/release-env.sh
+++ dev/release-env.sh
@@ -2,8 +2,8 @@
 
 export DEV_INITIALS=uvb
 export RELEASE_MAJOR_MINOR=4.10
-export RELEASE_PATCH=1
-export RELEASE_PRE='' # '-RC.1'; or '' for stable release
+export RELEASE_PATCH=2
+export RELEASE_PRE='-RC.1' # '-RC.1'; or '' for stable release
 export VERSION_PREFIX='v'
 
 export RELEASE_MAIN="$RELEASE_MAJOR_MINOR.$RELEASE_PATCH"
