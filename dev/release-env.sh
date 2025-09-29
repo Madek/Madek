b435515ff62ad9cda3d5473a41ccb20633aa0a74
uvb <urs.vanbinsbergen@zhdk.ch>
2025-09-29 10:36:15 +0200

    release: v4.10.2

--- dev/release-env.sh
+++ dev/release-env.sh
@@ -3,7 +3,7 @@
 export DEV_INITIALS=uvb
 export RELEASE_MAJOR_MINOR=4.10
 export RELEASE_PATCH=2
-export RELEASE_PRE='-RC.1' # '-RC.1'; or '' for stable release
+export RELEASE_PRE='' # '-RC.1'; or '' for stable release
 export VERSION_PREFIX='v'
 
 export RELEASE_MAIN="$RELEASE_MAJOR_MINOR.$RELEASE_PATCH"
