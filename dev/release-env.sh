94776a3ce1ed353f60ba07b354fc3d47d233a31d
uvb <urs.vanbinsbergen@zhdk.ch>
2026-07-06 09:08:59 +0200

    release: v4.15.0-RC.1

--- dev/release-env.sh
+++ dev/release-env.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -exu
 
 export DEV_INITIALS=uvb
-export RELEASE_MAJOR_MINOR=4.14
+export RELEASE_MAJOR_MINOR=4.15
 export RELEASE_PATCH=0
-export RELEASE_PRE='' # '-RC.1'; or '' for stable release
+export RELEASE_PRE='-RC.1' # '-RC.1'; or '' for stable release
 export VERSION_PREFIX='v'
 
 export RELEASE_MAIN="$RELEASE_MAJOR_MINOR.$RELEASE_PATCH"
